a75d8bb195baee662ab697ba86bd200832c32e00
Irma Gerguri <irge24@student.bth.se>
2026-06-21 11:38:25 +0200

    Test valid user without tasks

--- backend/test/demo/test_dao.py
+++ backend/test/demo/test_dao.py
@@ -9,3 +9,47 @@ def dao():
     yield dao     # ge den till testerna
 
     dao.drop()   # städa efter testerna
+
+def test_valid_user_without_tasks(dao):
+    user = {
+        "firstName": "Anna",
+        "lastName": "Andersson",
+        "email": "unique1@gmail.com", 
+        "tasks": []
+    }
+
+    result = dao.create(user)
+
+    assert "firstName" in result
+    assert result["tasks"] == []
+
+def test_missing_firstName(dao):
+    pass
+
+
+def test_missing_lastName(dao):
+    pass
+
+
+def test_missing_email(dao):
+    pass
+
+
+def test_firstName_wrong_type(dao):
+    pass
+
+
+def test_lastName_wrong_type(dao):
+    pass
+
+
+def test_email_wrong_type(dao):
+    pass
+
+
+def test_duplicate_email(dao):
+    pass
+
+
+def test_tasks_wrong_datatype(dao):
+    pass
