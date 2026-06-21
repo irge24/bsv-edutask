83a9dada712c6eaef61cbcd47215269de82d1bb7
Irma Gerguri <irge24@student.bth.se>
2026-06-21 11:30:13 +0200

    Pytest fixture

--- backend/test/demo/test_dao.py
+++ backend/test/demo/test_dao.py
@@ -1,1 +1,11 @@
 import pytest
+from src.util.dao import DAO
+
+@pytest.fixture
+def dao():
+    dao = DAO("user")
+    dao.drop()   # starta rent
+
+    yield dao     # ge den till testerna
+
+    dao.drop()   # städa efter testerna
